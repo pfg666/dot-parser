1e35f9f62dc868b7625603455b44df156d2c1bf3
Paul Fiterau <fiteraup@yahoo.com>
2020-11-17 14:53:37 +0100

    Updated tests + models

--- src/test/java/com/pfg666/dotparser/fsm/dfa/DFADotParserTest.java
+++ src/test/java/com/pfg666/dotparser/fsm/dfa/DFADotParserTest.java
@@ -12,7 +12,8 @@ import net.automatalib.automata.fsa.impl.FastDFA;
 import net.automatalib.automata.fsa.impl.FastDFAState;
 
 public class DFADotParserTest {
-private static String DTLS_CLIENT="/models/dtls_client.dot";
+	private static String DTLS_CLIENT="/models/dfa/dtls_client.dot";
+	private static String DTLS_SERVER="/models/dfa/dtls_server.dot";
 	
 	private DFADotParser<String> stringParser;
 	
@@ -22,13 +23,12 @@ private static String DTLS_CLIENT="/models/dtls_client.dot";
 	}
 	
 	@Test
-	public void dfaTest() throws Exception{
+	public void dfaDtlsClientTest() throws Exception{
 		InputStream stream = DFADotParserTest.class.getResource(DTLS_CLIENT).openStream();
 		List<FastDFA<String>> automata = stringParser.parseAutomaton(stream);
 		Assert.assertEquals(1, automata.size());
 		FastDFA<String> aut = automata.get(0);
 		Assert.assertEquals(18, aut.getStates().size());
-		System.out.println(aut.getInputAlphabet());
 		Assert.assertEquals(16, aut.getInputAlphabet().size());
 		FastDFAState init = aut.getInitialState();
 		Assert.assertFalse(init.isAccepting());
@@ -42,6 +42,30 @@ private static String DTLS_CLIENT="/models/dtls_client.dot";
 				"?CHANGE_CIPHER_SPEC",
 				"?FINISHED"
 				));
+		Assert.assertTrue(state.isAccepting());
+	}
+	
+	@Test
+	public void dfaDtlsServerTest() throws Exception{
+		InputStream stream = DFADotParserTest.class.getResource(DTLS_SERVER).openStream();
+		List<FastDFA<String>> automata = stringParser.parseAutomaton(stream);
+		Assert.assertEquals(1, automata.size());
+		FastDFA<String> aut = automata.get(0);
+		Assert.assertEquals(12, aut.getStates().size());
+		Assert.assertEquals(10, aut.getInputAlphabet().size());
+		FastDFAState init = aut.getInitialState();
+		Assert.assertFalse(init.isAccepting());
+		FastDFAState state = aut.getState(Arrays.asList(
+				"?PSK_CLIENT_HELLO",
+				"!HELLO_VERIFY_REQUEST",
+				"?PSK_CLIENT_HELLO",
+				"!SERVER_HELLO",
+				"!SERVER_HELLO_DONE",
+				"?PSK_CLIENT_KEY_EXCHANGE",
+				"?CHANGE_CIPHER_SPEC",
+				"?FINISHED",
+				"!CHANGE_CIPHER_SPEC",
+				"!FINISHED" ));
 		Assert.assertTrue(state.isAccepting());
 	}
 }
